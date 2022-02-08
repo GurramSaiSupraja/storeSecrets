2c24d65b14d7412f33316893b3fc5cbbea7a4600
saisupraja <saisupraja.gurram@gmail.com>
2022-02-08 20:32:41 +0530

    Used md5-hashing for authentication

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 const app = express();
 
 app.use(express.static("public"));
@@ -16,7 +16,6 @@ const userSchema = mongoose.Schema({email: String,
                                     password: String});
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 
 const User = new mongoose.model("User", userSchema);
@@ -34,7 +33,7 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({email: req.body.username , password: req.body.password});
+  const newUser = new User({email: req.body.username , password: md5(req.body.password)});
   newUser.save(function(err){
     if(!err){
         res.render("secrets");
@@ -46,7 +45,7 @@ app.post("/login", function(req, res){
   User.findOne({email: req.body.username}, function(err, foundUser){
     if(!err){
       if(foundUser){
-        if(foundUser.password === req.body.password)
+        if(foundUser.password === md5(req.body.password))
           res.render("secrets");
         else{
           console.log("Incorrect pwd");
